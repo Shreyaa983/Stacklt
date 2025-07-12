5c70a334fcc7c18f6dd84c7f379bdb04654ac696
Shreya Shukla <shreyashukla20042005@gmail.com>
2025-07-12 12:29:36 +0530

    login

--- stackit/backend/src/app.js
+++ stackit/backend/src/app.js
@@ -1,30 +1,13 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes';
-import questionRoutes from './routes/questionRoutes';
-import answerRoutes from './routes/answerRoutes';
-import notificationRoutes from './routes/notificationRoutes';
-import adminRoutes from './routes/adminRoutes';
-
 const express = require('express');
 const cors = require('cors');
-
 const authRoutes = require('./routes/authRoutes');
-const questionRoutes = require('./routes/questionRoutes');
-const answerRoutes = require('./routes/answerRoutes');
-const notificationRoutes = require('./routes/notificationRoutes');
-const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Mount login route
 app.use('/api/auth', authRoutes);
-app.use('/api/questions', questionRoutes);
-app.use('/api/answers', answerRoutes);
-app.use('/api/notifications', notificationRoutes);
-app.use('/api/admin', adminRoutes);
 
 module.exports = app;
